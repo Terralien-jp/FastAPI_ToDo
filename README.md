c951b9f3a2cfed78362d52fee4740038ed0cd804
Terralien-jp <terralienjp@gmail.com>
2022-05-06 07:46:26 +0900

    fix crud

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FastAPI_ToDo
+
+## Doneリソース
+
 https://zenn.dev/sh0nk/books/537bb028709ab9/viewer/b92ab0
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,6 @@
+from curses.ascii import HT
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
@@ -13,6 +14,7 @@ router = APIRouter()
 async def list_tasks(db: AsyncSession = Depends(get_db)):
     return await task_crud.get_tasks_with_done(db)
 
+
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(
     task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
@@ -21,10 +23,20 @@ async def create_task(
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+async def update_task(
+    task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.update_task(db, task_body, original=task)
 
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int):
-    return
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.delete_task(db, original=task)
